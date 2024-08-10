c5b1a83baeef8d87e086cbb12ff81c26b1087350
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2024-08-10 01:02:02 +0100

    Enviando mensagem localemente

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -7,5 +7,9 @@ class ChatProvider extends ChangeNotifier {
     Message(text: 'Já voltasse do trabalho', fromWho: FromWho.me),
   ];
 
-  Future<void> sendMessage(String text) async {}
+  Future<void> sendMessage(String text) async {
+    final newMessage = Message(text: text, fromWho: FromWho.me);
+    messageList.add(newMessage);
+    notifyListeners();
+  }
 }
